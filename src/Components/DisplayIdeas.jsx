35b45a02e83dedda7a5b7327ff8da09cd7847c9b
JacksenSE <Jacskenbmarshall@gmail.com>
2023-01-04 19:50:51 -0800

    solved

---
+++ src/Components/DisplayIdeas.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function DisplayIdeas (props) {
+  return(
+  
+    <ul>
+        {props.ideas.map((idea, i)=><li key={i}>{idea}</li>)}
+    </ul>
+  )
+}
+
+export default DisplayIdeas
